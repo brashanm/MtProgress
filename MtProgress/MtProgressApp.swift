f0ec278bf1f1a36af3c9afe2e694fbb5c3914af6
Brashan <brashanm@gmail.com>
2024-01-04 14:33:47 -0500

    Initial Commit

---
+++ MtProgress/MtProgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  MtProgressApp.swift
+//  MtProgress
+//
+//  Created by Brashan Mohanakumar on 2024-01-04.
+//
+
+import SwiftUI
+
+@main
+struct MtProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
